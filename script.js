5b577ea1a91627272fe86d52a29e3bb62a185e8b
Maria-source-cell <manuelabirnat@gmail.com>
2025-07-30 11:39:27 +0200

    boiler template

---
+++ script.js
@@ -0,0 +1,9 @@
+//function called computerPlay that will randomly return either ‘Rock’,‘Paper’ or ‘Scissors’.
+
+//Write a function that plays a single round of Rock Paper Scissors.The function should take two parameters - the playerSelection and computer.Selection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock". ---Make your function’s playerSelection parameter case￾insensitive (
+
+//function called game(). Call the playRound function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end. use loop, max 5 rounds
+
+// use prompt to get input from the user
+
+//pay attention to user experience, fun text, backstory. Clear instruction
